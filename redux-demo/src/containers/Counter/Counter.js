9d143fc65a0a41ddd3484553f77e85b91efc0c03
VUTuanTrung <trung.lespace@gmail.com>
2021-01-05 22:50:39 +0100

    finish redux example

--- redux-demo/src/containers/Counter/Counter.js
+++ redux-demo/src/containers/Counter/Counter.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionType from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-            default: break;
-        }
-    }
-
     render() {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler('inc')} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store result</button>
+                <ul>
+                    {this.props.storedResults.map((strResult) => {
+                        return (
+                            <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult}</li>
+                        )
+                    })}
+                </ul>
+
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: actionType.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionType.DECREMENT }),
+        onAddCounter: () => dispatch({ type: actionType.ADD, val: 10, }),
+        onSubtractCounter: () => dispatch({ type: actionType.SUBTRACT, val: 15, }),
+        onStoreResult: (result) => dispatch({ type: actionType.STORE_RESULT, result: result }),
+        onDeleteResult: (id) => dispatch({ type: actionType.DELETE_RESULT, resultElId: id, }),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ redux-demo/src/store/reducers/result.js
@@ -0,0 +1,33 @@
+import * as actionType from '../actions';
+
+const initialState = {
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionType.STORE_RESULT:
+            console.log(state)
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(),
+                    value: action.result
+                }),
+            };
+        case actionType.DELETE_RESULT:
+            const updatedArray = state.results.filter(item => item.id !== action.resultElId);
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(),
+                    value: updatedArray
+                })
+            };
+        default:
+            break;
+    }
+    return state;
+};
+
+export default reducer;
