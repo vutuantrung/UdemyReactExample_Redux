9838f7f36ec80c9bf2293ab3c75539b63b66d841
VUTuanTrung <trung.lespace@gmail.com>
2021-01-06 18:44:56 +0100

    code review

--- redux-demo/src/store/reducers/counter.js
+++ redux-demo/src/store/reducers/counter.js
@@ -8,21 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionType.INCREMENT:
-            return updateObject(state, {
-                counter: state.counter + 1
-            })
+            return updateObject(state, { counter: state.counter + 1 })
         case actionType.DECREMENT:
-            return updateObject(state, {
-                counter: state.counter - 1
-            })
+            return updateObject(state, { counter: state.counter - 1 })
         case actionType.ADD:
-            return updateObject(state, {
-                counter: state.counter + action.val
-            })
+            return updateObject(state, { counter: state.counter + action.val })
         case actionType.SUBTRACT:
-            return updateObject(state, {
-                counter: state.counter - action.val
-            })
+            return updateObject(state, { counter: state.counter - action.val })
         default:
             return state;
     }
